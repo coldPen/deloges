e85cdda4b910345d3d0b1cc264c4976eceb93d91
cahnory <cahnory@gmail.com>
2019-01-19 20:54:35 +0100

    add static files controller

--- server/app.js
+++ server/app.js
@@ -5,5 +5,6 @@ const commonMiddleware = require('./middlewares/common');
 const app = express();
 app.use(commonMiddleware);
 app.use('/api/v1', api);
+app.use('/assets', express.static('public'));
 
 module.exports = app;
