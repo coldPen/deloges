fc273e6fe364efd2965482b876c3419c6e8ec3a3
cahnory <cahnory@gmail.com>
2019-01-20 12:24:08 +0100

    add create-database script

--- db/tables.sql
+++ db/tables.sql
@@ -1,49 +1,54 @@
 
 CREATE TABLE users (
-    id BIGINT,
-    date_create TIMESTAMP,
-    date_modify TIMESTAMP,
-    login TEXT,
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    date_create TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    date_modify TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    login VARCHAR(100),
     user_type VARCHAR(100),
-    email TEXT,
+    email VARCHAR(100),
     phonenumber VARCHAR(100),
-    name TEXT ,
-    firstname TEXT,
+    name VARCHAR(100),
+    firstname VARCHAR(100),
     genre VARCHAR(100),
     address TEXT,
-    geolocalisation SPATIAL,
-    rgpdack TIMESTAMP,
+    geolocalisation INT,
+    rgpdack TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    password VARCHAR(100) DEFAULT NULL,
+    PRIMARY KEY (`id`)
 );
 
 
 CREATE TABLE offers (
-    id BIGINT,
-    date_create TIMESTAMP,
-    date_modify TIMESTAMP,
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    date_create TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    date_modify TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     user_id BIGINT,
     offer_type VARCHAR(100),
-    datestart TIMESTAMP,
-    dateend TIMESTAMP,
-    active TIMESTAMP,
+    datestart TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    dateend TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    active TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     description TEXT,
+    PRIMARY KEY (`id`)
 );
 
 CREATE TABLE asks (
-    id BIGINT,
-    date_create TIMESTAMP,
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    date_create TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     asker_id BIGINT,
     benevole_id BIGINT,
     offer_id BIGINT,
     message_id BIGINT,
     status VARCHAR(100),
+    PRIMARY KEY (`id`)
 );
 
 CREATE TABLE messages (
-    id BIGINT,
-    date_create TIMESTAMP,
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    date_create TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     parent_id BIGINT,
     message_type VARCHAR(100),
     title TEXT,
     body TEXT,
+    PRIMARY KEY (`id`)
 );
 
