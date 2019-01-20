66a8869a07dc8e404e02709a8efdc086e5ac19f8
cahnory <cahnory@gmail.com>
2019-01-20 13:44:28 +0100

    add access control on dislodged signup-token

--- server/api/dislodged/index.js
+++ server/api/dislodged/index.js
@@ -1,9 +1,14 @@
 const Router = require('express-promise-router');
+const requireAdminMiddleware = require('../../middlewares/requireAdmin');
 const dislodgedSignupToken = require('./signupToken');
 const dislodgedSignup = require('./signup');
 
 const apiVolunteer = Router();
-apiVolunteer.post('/signup-token', dislodgedSignupToken);
+apiVolunteer.post(
+  '/signup-token',
+  requireAdminMiddleware,
+  dislodgedSignupToken
+);
 apiVolunteer.post('/signup', dislodgedSignup);
 
 module.exports = apiVolunteer;
